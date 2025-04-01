4bf2a5044d1f1224913a50856a28085405a21fc3
michael411302225 <u11302225@o365.ttu.edu.tw.com>
2025-04-01 21:17:39 +0800

    把控制愛心次數的for迴圈換成while迴圈

---
+++ HW1.3.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	int a,i=0;
+	char ch;
+	while(i<=1){
+	
+	printf("    **   **   \n");
+    printf(" *************  \n");
+    printf("***************\n");
+    printf(" *************\n");
+    printf("  ***********  \n");
+    printf("   *********   \n");
+    printf("    *******    \n");
+    printf("     *****     \n");
+    printf("      ***     \n");
+    printf("       *   \n");
+    i++;
+	}
+
+	system("pause");
+	system("CLS");
+	printf("輸入 4 個數字的密碼:");
+	scanf("%d",&a);
+	if(a==2024){
+		printf("歡迎\n");
+		system("pause");
+		system("CLS");
+		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf("|  'A'......'Z' : Uppercase  |\n");
+		printf("|  'a'......'z' : Lowercase  |\n");
+		printf("|  '0'......'9' : Digit      |\n");
+		printf("|   Otherwise   : Your name  |\n");
+		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf("輸入1個字元:");
+		fflush(stdin);
+		scanf("%c",&ch);
+		if(ch>='A'&&ch<='Z'){
+			printf("Uppercase");
+			system("pause");
+		}
+		else if(ch>='a'&&ch<='z'){
+			printf("Lowercase");
+			system("pause");
+		}
+		else if(ch>='0'&&ch<='9'){
+			printf("Digit");
+			system("pause");
+		}
+		else{
+			printf("E1B25何弘煒");
+			system("pause");
+		}
+	}
+	else{
+		printf("輸入錯誤");
+	}
+	return 0;
+} 
