327d8dce12930a5b3dd7281a6df0020640da265d
michael411302225 <u11302225@o365.ttu.edu.tw.com>
2025-04-29 01:53:46 +0800

    第一部分

---
+++ HW2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	int a,i=0;
+	char ch;
+	while(i<=1){
+	
+	printf("    **   **   \n");
+    printf(" *************  \n");
+    printf("***************\n");
+    printf(" *************\n");
+    printf("  ***********  \n");
+    printf("   *********   \n");
+    printf("    *******    \n");
+    printf("     *****     \n");
+    printf("      ***     \n");
+    printf("       *   \n");
+    i++;
+	}
+
+	system("pause");
+	system("CLS");
+	printf("輸入 4 個數字的密碼:");
+	scanf("%d",&a);
+	if(a==2025){
+		
+	} 
