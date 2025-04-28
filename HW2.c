f32a26b7e8d8c64bc12256b8384b092e3dd7715e
michael411302225 <u11302225@o365.ttu.edu.tw.com>
2025-04-29 02:21:46 +0800

    加入密碼限制和選單

--- HW2.c
+++ HW2.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 int main(void){
 	int a,i=0;
-	char ch;
 	while(i<=1){
 	
 	printf("    **   **   \n");
@@ -17,11 +16,32 @@ int main(void){
     printf("       *   \n");
     i++;
 	}
-
 	system("pause");
 	system("CLS");
-	printf("輸入 4 個數字的密碼:");
-	scanf("%d",&a);
+	for(i=1;i<=3;i++){
+		printf("輸入 4 個數字的密碼(2025):");
+		scanf("%d",&a);
+		if(a==2025){
+			
+			printf("密碼正確.\n");
+			break;
+			
+		}
+		else{
+			printf("密碼錯誤.\n");
+			
+		}
+	}
 	if(a==2025){
-		
-	} 
+			printf("~~~~~~~~~~~~~~~~~~~\n");
+			printf("|  a. 畫出三角形  |\n");
+			printf("|  b. 顯示乘法表  |\n");
+			printf("|  c. 結束        |\n");
+			printf("~~~~~~~~~~~~~~~~~~~\n");
+			
+	}
+	system("pause");
+	return 0;
+}
+
+
