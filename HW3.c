a85091d95253352e5a7d3386b6f6e7b6b6232d76
michael411302225 <u11302225@o365.ttu.edu.tw.com>
2025-05-11 01:53:59 +0800

    Set personal style pattern, password, directory

---
+++ HW3.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	int a,i=0,n,l=0,w=0;
+	char ch,ch1,j,k,ch2;
+	while(i<=1){
+	
+	printf("*******************\n");
+    printf(" *****************\n");
+    printf("  ***************\n");
+    printf("   *************\n");
+    printf("    ***********  \n");
+    printf("     *********   \n");
+    printf("      *******    \n");
+    printf("       *****     \n");
+    printf("        ***     \n");
+    printf("         *   \n");
+    i++;
+	}
+	system("pause");
+	system("CLS");
+	for(i=1;i<=3;i++){
+		printf("¿é¤J 4 ­Ó¼Æ¦rªº±K½X(2025):");
+		scanf("%d",&a);
+		if(a==2025){
+			
+			printf("±K½X¥¿½T.\n");
+			printf("Welcome\n");
+			break;
+			
+		}
+		else{
+			printf("±K½X¿ù»~.\n");
+			w++;
+			
+			
+		}
+	}
+	system("pause");
+	system("CLS");
+	if(w==3){
+		return 0;
+	}
+	
+
+	printf("------[Booking System]------\n");
+	printf("|  a. Available            |\n");
+	printf("|  b. Arrange for you      |\n");
+	printf("|  c. Exit                 |\n");
+	printf("----------------------------\n");
+			
+			
+	
+	system("pause");
+	return 0;
+}
+
+
