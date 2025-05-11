53f84513d39a6fb80990e693ae1140f020585a55
michael411302225 <u11302225@o365.ttu.edu.tw.com>
2025-05-11 23:30:04 +0800

    Make a seating chart

--- HW3.c
+++ HW3.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
 int main(void){
-	int a,i=0,n,l=0,w=0;
-	char ch,ch1,j,k,ch2;
+	int a,i=0,w=0,j,t,row,col;
+	char ch;
+	char m[9][9];
+	char p[81];
+	srand(time(NULL));
+	
 	while(i<=1){
 	
 	printf("*******************\n");
@@ -51,7 +56,36 @@ int main(void){
 	printf("----------------------------\n");
 	fflush(stdin); 
 	scanf("%c",&ch);
+	
 	if(ch=='a'||ch=='A'){
+		for(i=0;i<9;i++){
+			for(j=0;j<9;j++){
+				m[i][j]='-';
+			}
+		}
+		for(i=0;i<81;i++){
+			p[i]=i;
+		}
+		for(i=80;i>0;i--){
+			j=rand()%(i+1);
+			t=p[i];
+			p[i]=p[j];
+			p[j]=t;
+		}
+		for(i=0;i<10;i++){
+			row=p[i]/9;
+			col=p[i]%9;
+			m[row][col]='*';
+		}
+		system("CLS");
+		printf("\\123456789\n");
+		for(i=8;i>=0;i--){
+			printf("%d",i+1);
+			for(j=0;j<9;j++){
+				printf("%c",m[i][j]);
+			}
+			printf("\n");
+		}
 		
 	}
 			
